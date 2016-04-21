e836e532d55d9c1cd45f18185e86117fef539cf3
Lyell Brown <lyellbrown91@gmail.com>
2016-04-20 21:21:10 -0500

    Init repo

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');		// require express module
+var app = express();		// create express app, calls 'express' as a function
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	res.send('Todo API ROOT');
+});
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!')
+});
